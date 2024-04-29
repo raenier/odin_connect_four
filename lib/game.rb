38ffb58533a302b8c0743441eef087d59ca04c4f
ren <raenierquiambao@gmail.com>
2024-04-29 15:35:11 +0800

    Create players on initialization

--- lib/game.rb
+++ lib/game.rb
@@ -1,11 +1,13 @@
 require 'board'
+require 'player'
 
 class Game
-  attr_reader :board
+  attr_reader :board, :players
 
   BOARD_SIZE = [6, 7].freeze
 
   def initialize
     @board = Board.new(*BOARD_SIZE)
+    @players = [Player.new('white'), Player.new('black')]
   end
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -7,5 +7,10 @@ describe Game do
     it 'creates one board' do
       expect(game.board).to be_a Board
     end
+
+    it 'creates two players' do
+      expect(game.players.first).to be_a Player
+      expect(game.players.last).to be_a Player
+    end
   end
 end
